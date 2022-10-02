cc1cfd73e265eebff98386ee9ae818ded65fdfd4
Wilson Wu <105242601+wilsonwugh@users.noreply.github.com>
2022-10-02 16:22:53 -0500

    AKS_00 - Creating Kafka Project
    kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic demo_java

---
+++ Kafka-basics/src/main/java/io/conduktor/demo/ProducerDemo.java
@@ -0,0 +1,38 @@
+package io.conduktor.demo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Hello Word!");
+
+        // create Producer Property
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String,String> producer =
+                new KafkaProducer<>(properties);
+        // crate producer record
+        ProducerRecord<String,String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello_world");
+        // send data - asynchronous
+        producer.send(producerRecord);
+        // flush data - synchronous
+        producer.flush();
+        // flush and close the Producer
+        producer.close();
+
+    }
+}
